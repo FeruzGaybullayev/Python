2dd5b2fba7df18864e9a3160f968892fa9fe23d7
FeruzGaybullayev <gaybullayevferuzjon108@gmail.com>
2024-12-24 12:38:13 +0500

    hello

---
+++ lesson-1/third.py
@@ -0,0 +1,9 @@
+l_e_n = 5
+second_number = 10
+
+print(l_e_n + second_number)
+
+
+
+x = len("hello")
+print(x)
---
+++ lesson-2/simple_check_app.py
@@ -0,0 +1,6 @@
+word = input("Enter a ____ : ")
+
+if not bool(word.strip()) :
+    print ('Please enter something : ')
+else: 
+    print('You entered : ', word)
