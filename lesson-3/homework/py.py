69295d57f5bd61955e0033176ac6a8f83dabb597
FeruzGaybullayev <gaybullayevferuzjon108@gmail.com>
2024-12-30 12:07:25 +0500

    lesson-3

--- lesson-3/homework/py.py
+++
@@ -1,7 +0,0 @@
-list_of_elements = input("Enter a list (elements separated by space): ").split()
-n = input("Elementni kiriting: ")
-
-# Element qanchalik ko'p marta uchrayotganini hisoblash
-count = list_of_elements.count(n)
-
-print(f"{n} ro'yxatda {count} marta uchraydi.")
